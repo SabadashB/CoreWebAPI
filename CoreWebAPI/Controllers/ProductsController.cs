ea77c16161018285c643ab028bba4e7d90b58eab
bogdan-2025 <bogdan-2025@ukr.net>
2022-02-19 09:44:49 +0200

    Add project files.

---
+++ CoreWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using Core1WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core1WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> _products;
+        private readonly ILogger<ProductsController> _logger;
+
+        static ProductsController()
+        {
+            _products = new List<Product>();
+        }
+
+        public ProductsController(
+            ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            product.Id = Guid.NewGuid();
+            _products.Add(product);
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts(Product user)
+        {
+            return Ok(_products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(Guid id)
+        {
+            Product product = _products.FirstOrDefault(user => user.Id == id);
+            if(product != null)
+            {
+                return Ok(product);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(Product product)
+        {
+            var dbProduct = _products.FirstOrDefault(x => x.Id == product.Id);
+            if(dbProduct != null)
+            {
+                var index = _products.IndexOf(dbProduct);
+                _products[index] = product;
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(Guid id)
+        {
+            Product dbProduct = _products.FirstOrDefault(x => x.Id == id);
+            if(dbProduct != null)
+            {
+                _products.Remove(dbProduct);
+
+                return Ok(dbProduct);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ CoreWebAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core1WebAPI.Models
+{
+    public class Product
+    {
+        [KeyAttribute]
+        public Guid Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(25)]
+        public string Name { get; set; }
+        [Required]
+        public int Count { get; set; }
+        [Required]
+        public DateTime ArrivedDate { get; set; }
+
+    }
+}
